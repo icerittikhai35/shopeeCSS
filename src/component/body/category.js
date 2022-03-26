1f55f59573276119c9736401fd914b86c3ef0159
icerittikhai35 <ritthikai.khan@gmai.com>
2022-03-26 14:41:58 +0700

    Update catagory

--- src/component/body/category.js
+++ src/component/body/category.js
@@ -27,99 +27,187 @@ function SamplePrevArrow(props) {
 
 function Catagory() {
     var settings = {
-        dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-       
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        initialSlide: 0,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
 
 
 
     };
     return (
         <div style={styles.container}>
-            <div style={{ width: '98%', margin: 'auto', paddingTop: 20, }}>
-                <label style={{ fontSize: 18, fontWeight: 'normal', color: '#99938e' }}>หมวดหมู่</label>
+            <div style={{ width: '100%', margin: 'auto', paddingTop: 20, }}>
+                <label style={{ fontSize: 18, fontWeight: 'normal', color: '#99938e', paddingLeft: 40, paddingBottom: 10 }}>หมวดหมู่</label>
                 <Slider {...settings}>
-                    <div style={{ margin: 'auto', display: 'flex', justifyContent: 'space-between' }}>
-                        <div style={{ width: '10%', backgroundColor: '#f5f5f5', margin: 'auto', display: 'flex', flexDirection: 'column', marginBottom: 0 }}>
-                            <div style={{ margin: 'auto' }}>
-                                <img src='https://cf.shopee.co.th/file/8016bce97bececb1cc323ddd28f5c6fd' alt="" width={70} style={{ borderRadius: 50 }} />
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
                             </div>
-                            <div style={{ margin: 'auto' }}>
-                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>เสื้อผ้าแฟรชั่นผู้ชาย</label>
-                            </div>
-                        </div>
-                        <div style={{ width: '10%', backgroundColor: '#f5f5f5', margin: 'auto', display: 'flex', flexDirection: 'column' }}>
-                            <div style={{ margin: 'auto' }}>
-                                <img src='https://cf.shopee.co.th/file/8016bce97bececb1cc323ddd28f5c6fd' alt="" width={70} style={{ borderRadius: 50 }} />
-                            </div>
-                            <div style={{ margin: 'auto' }}>
-                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>เสื้อผ้าแฟรชั่นผู้ชาย</label>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
                             </div>
                         </div>
+
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>2</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>2</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>2</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>3</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>3</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>3</h3>
+
+                    <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
+                        <div style={styles.boxCatagory}>
+                            <div style={styles.BoxImgCatagory}>
+                                <img src='https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd' alt="" width={70} style={{ borderRadius: 50 }} />
+                            </div>
+                            <div style={styles.BoxImgCatagory}>
+                                <label style={{ fontSize: 14, fontWeight: 'normal', color: '#000000' }}>คอมพิวเตอร์</label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                   
+
+
+
                 </Slider>
             </div>
 
@@ -142,6 +230,18 @@ const styles = {
     },
     showBullets: {
         color: 'red'
+    },
+    boxCatagory: {
+        width: '100%',
+        backgroundColor: '#ffffff',
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        border: '0.01px solid #f2f2f2'
+    },
+    BoxImgCatagory: {
+        margin: 'auto',
+        marginTop: 15
     },
 }
 export default Catagory;
