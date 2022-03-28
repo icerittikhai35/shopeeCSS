6430de4e5c205d018fd6fd6e7e2b855b82375d91
icerittikhai35 <ritthikai.khan@gmai.com>
2022-03-29 00:30:55 +0700

    Update flashSale.js

--- src/component/body/flashSale.js
+++ src/component/body/flashSale.js
@@ -11,11 +11,11 @@ function FlashSale() {
                         <img src={FlashSaleImg} alt="" width={'100px'} />
 
                     </div>
-                    <div style={{ fontSize: 16, fontWeight: 'bold', paddingTop: 5 ,color:'white',backgroundColor:'black'}}>
+                    <div style={{ fontSize: 16, fontWeight: 'bold', paddingTop: 5, color: 'white', backgroundColor: 'black', width: '80px', textAlign: 'center' }}>
                         <Countdown
-                         date={Date.now() + 100000}
-                         daysInHours={true}
-                          />
+                            date={Date.now() + 100000}
+                            daysInHours={true}
+                        />
                     </div>
                 </div>
 
@@ -23,26 +23,44 @@ function FlashSale() {
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://th-live-05.slatic.net/p/abe3987bdd0d572b39933268fcbf6487.jpg_720x720q80.jpg_.webp" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿1,500 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://cf.shopee.co.th/file/23e367905146d76c67e2308e7abd2f6d" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿2,490 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://cf.shopee.co.th/file/beaca331d8f31b9c629d8382671918e0" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿500 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://cf.shopee.co.th/file/6c414275f7cff2e59c0acfa8cf6c4edd" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿5,590 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://cf.shopee.co.th/file/62c03f1a22bd224a4c84ddc2febfc9ca" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿9,990 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                     <div style={{ width: '15%', height: '210px', backgroundColor: '#ffffff', textAlign: 'center' }}>
                         <img src="https://cf.shopee.co.th/file/6073fad9363f77893e96a7ae0ec5960a" alt="new" width={'100%'} />
                         <label style={styles.textPrice}>฿590 </label>
+                        <div style={{ width: '90%', backgroundColor: '#ffbda6', height: 20, borderRadius: 100, margin: 'auto', }}>
+                            <label style={{ fontSize: 12, color: 'white', textAlign: 'center', fontWeight: 'normal', paddingBottom: 10 }}>ขายแแล้ว 0 </label>
+                        </div>
                     </div>
                 </div>
 
@@ -59,7 +77,7 @@ const styles = {
         margin: 'auto',
         marginTop: 20,
         boxShadow: '0px 0px 0px #000000',
-        height: '350px',
+        height: '300px',
     },
     containerBox: {
         maxWidth: '1200px',
